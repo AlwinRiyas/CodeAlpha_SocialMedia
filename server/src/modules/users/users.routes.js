ff7448a6085130bbf46ba058d950a2df6e858a5c
Alwinriyas <alwinriyas16@gmail.com>
2026-08-23 11:23:09 +0530

    feat(users): add protected profile update endpoint

--- server/src/modules/users/users.routes.js
+++ server/src/modules/users/users.routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { authenticate } from '../../middleware/authenticate.js'
-import { getUserProfile } from './users.service.js'
+import { getUserProfile, updateProfile } from './users.service.js'
 
 export const usersRouter = Router()
 
@@ -8,11 +8,16 @@ usersRouter.get('/me', authenticate, (req, res) => {
   res.status(200).json({ data: req.user })
 })
 
+usersRouter.patch('/me', authenticate, async (req, res, next) => {
+  try {
+    const user = await updateProfile(req.user.id, req.body)
+    res.status(200).json({ data: user })
+  } catch (error) { next(error) }
+})
+
 usersRouter.get('/:username', async (req, res, next) => {
   try {
     const user = await getUserProfile(req.params.username)
     res.status(200).json({ data: user })
-  } catch (error) {
-    next(error)
-  }
+  } catch (error) { next(error) }
 })
