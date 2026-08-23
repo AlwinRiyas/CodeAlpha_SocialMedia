c248a2ba4466f8262ce73408a5735a8fb083cbcd
Alwinriyas <alwinriyas16@gmail.com>
2026-08-23 11:30:53 +0530

    feat(realtime): pass Socket.IO to comment events

--- server/src/modules/comments/comments.routes.js
+++ server/src/modules/comments/comments.routes.js
@@ -3,14 +3,5 @@ import { authenticate } from '../../middleware/authenticate.js'
 import { createComment, listComments } from './comments.service.js'
 
 export const commentsRouter = Router()
-
-commentsRouter.get('/:postId', async (req, res, next) => {
-  try { res.status(200).json({ data: await listComments(req.params.postId) }) }
-  catch (error) { next(error) }
-})
-
-commentsRouter.post('/:postId', authenticate, async (req, res, next) => {
-  try {
-    res.status(201).json({ data: await createComment(req.user.id, req.params.postId, req.body) })
-  } catch (error) { next(error) }
-})
+commentsRouter.get('/:postId', async (req, res, next) => { try { res.status(200).json({ data: await listComments(req.params.postId) }) } catch (error) { next(error) } })
+commentsRouter.post('/:postId', authenticate, async (req, res, next) => { try { res.status(201).json({ data: await createComment(req.user.id, req.params.postId, req.body, req.app.get('io')) }) } catch (error) { next(error) } })
