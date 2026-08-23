91032689ca239af330541dc915cb274f2b3bf7d0
Alwinriyas <alwinriyas16@gmail.com>
2026-08-23 11:19:01 +0530

    feat(api): register follows and personalized feed modules

--- server/src/app.js
+++ server/src/app.js
@@ -6,6 +6,8 @@ import { env } from './config/env.js'
 import { errorHandler, notFoundHandler } from './middleware/error-handler.js'
 import { authRouter } from './modules/auth/auth.routes.js'
 import { commentsRouter } from './modules/comments/comments.routes.js'
+import { feedRouter } from './modules/feed/feed.routes.js'
+import { followsRouter } from './modules/follows/follows.routes.js'
 import { likesRouter } from './modules/likes/likes.routes.js'
 import { postsRouter } from './modules/posts/posts.routes.js'
 import { usersRouter } from './modules/users/users.routes.js'
@@ -23,6 +25,8 @@ app.use('/api/users', usersRouter)
 app.use('/api/posts', postsRouter)
 app.use('/api/comments', commentsRouter)
 app.use('/api/likes', likesRouter)
+app.use('/api/follows', followsRouter)
+app.use('/api/feed', feedRouter)
 
 app.use(notFoundHandler)
 app.use(errorHandler)
