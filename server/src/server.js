235c5fcdfd848eab30fd645bc628686f2d879fc8
Alwinriyas <alwinriyas16@gmail.com>
2026-08-23 11:29:35 +0530

    feat(realtime): initialize Socket.IO notification channel

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,26 @@
+import http from 'node:http'
+import { Server } from 'socket.io'
 import app from './app.js'
 import { env } from './config/env.js'
 
-const server = app.listen(env.port, () => {
+const httpServer = http.createServer(app)
+const io = new Server(httpServer, { cors: { origin: env.nodeEnv === 'production' ? process.env.CLIENT_URL : true } })
+
+io.on('connection', (socket) => {
+  socket.on('notification:subscribe', (userId) => {
+    if (typeof userId === 'string' && userId) socket.join(`user:${userId}`)
+  })
+})
+
+app.set('io', io)
+
+httpServer.listen(env.port, () => {
   console.log(`API server running on http://localhost:${env.port}`)
 })
 
 function shutdown(signal) {
   console.log(`${signal} received. Shutting down gracefully.`)
-  server.close(() => process.exit(0))
+  io.close(() => httpServer.close(() => process.exit(0)))
 }
 
 process.on('SIGINT', () => shutdown('SIGINT'))
