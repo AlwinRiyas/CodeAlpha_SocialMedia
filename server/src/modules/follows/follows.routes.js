03fda7ce0c293f3794c765bb461572108391e142
Alwinriyas <alwinriyas16@gmail.com>
2026-08-23 11:30:36 +0530

    feat(realtime): pass Socket.IO to follow events

--- server/src/modules/follows/follows.routes.js
+++ server/src/modules/follows/follows.routes.js
@@ -3,23 +3,7 @@ import { authenticate } from '../../middleware/authenticate.js'
 import { followUser, listFollowers, listFollowing, unfollowUser } from './follows.service.js'
 
 export const followsRouter = Router()
-
-followsRouter.get('/:userId/followers', async (req, res, next) => {
-  try { res.status(200).json({ data: await listFollowers(req.params.userId) }) }
-  catch (error) { next(error) }
-})
-
-followsRouter.get('/:userId/following', async (req, res, next) => {
-  try { res.status(200).json({ data: await listFollowing(req.params.userId) }) }
-  catch (error) { next(error) }
-})
-
-followsRouter.post('/:userId', authenticate, async (req, res, next) => {
-  try { res.status(200).json({ data: await followUser(req.user.id, req.params.userId) }) }
-  catch (error) { next(error) }
-})
-
-followsRouter.delete('/:userId', authenticate, async (req, res, next) => {
-  try { res.status(200).json({ data: await unfollowUser(req.user.id, req.params.userId) }) }
-  catch (error) { next(error) }
-})
+followsRouter.get('/:userId/followers', async (req, res, next) => { try { res.status(200).json({ data: await listFollowers(req.params.userId) }) } catch (error) { next(error) } })
+followsRouter.get('/:userId/following', async (req, res, next) => { try { res.status(200).json({ data: await listFollowing(req.params.userId) }) } catch (error) { next(error) } })
+followsRouter.post('/:userId', authenticate, async (req, res, next) => { try { res.status(200).json({ data: await followUser(req.user.id, req.params.userId, req.app.get('io')) }) } catch (error) { next(error) } })
+followsRouter.delete('/:userId', authenticate, async (req, res, next) => { try { res.status(200).json({ data: await unfollowUser(req.user.id, req.params.userId) }) } catch (error) { next(error) } })
