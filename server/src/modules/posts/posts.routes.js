0b7658ca1b1271cfb07017bdf517fc866dbe0bc0
Alwinriyas <alwinriyas16@gmail.com>
2026-08-23 11:44:08 +0530

    feat(validation): enforce post and pagination validation

--- server/src/modules/posts/posts.routes.js
+++ server/src/modules/posts/posts.routes.js
@@ -1,29 +1,10 @@
 import { Router } from 'express'
 import { authenticate } from '../../middleware/authenticate.js'
 import { createPostForUser, getFeed, getPost, removePost } from './posts.service.js'
+import { validatePagination, validatePostPayload } from './posts.validation.js'
 
 export const postsRouter = Router()
-
-postsRouter.get('/', async (req, res, next) => {
-  try {
-    const data = await getFeed({ cursor: req.query.cursor, limit: req.query.limit })
-    res.status(200).json({ data })
-  } catch (error) { next(error) }
-})
-
-postsRouter.get('/:postId', async (req, res, next) => {
-  try { res.status(200).json({ data: await getPost(req.params.postId) }) }
-  catch (error) { next(error) }
-})
-
-postsRouter.post('/', authenticate, async (req, res, next) => {
-  try { res.status(201).json({ data: await createPostForUser(req.user.id, req.body) }) }
-  catch (error) { next(error) }
-})
-
-postsRouter.delete('/:postId', authenticate, async (req, res, next) => {
-  try {
-    await removePost(req.user.id, req.params.postId)
-    res.status(204).send()
-  } catch (error) { next(error) }
-})
+postsRouter.get('/', async (req, res, next) => { try { const data = await getFeed(validatePagination(req.query)); res.status(200).json({ data }) } catch (error) { next(error) } })
+postsRouter.get('/:postId', async (req, res, next) => { try { res.status(200).json({ data: await getPost(req.params.postId) }) } catch (error) { next(error) } })
+postsRouter.post('/', authenticate, async (req, res, next) => { try { res.status(201).json({ data: await createPostForUser(req.user.id, validatePostPayload(req.body)) }) } catch (error) { next(error) } })
+postsRouter.delete('/:postId', authenticate, async (req, res, next) => { try { await removePost(req.user.id, req.params.postId); res.status(204).send() } catch (error) { next(error) } })
