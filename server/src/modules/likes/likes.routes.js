57eb45246e1608dadf4f31554a7bc42546e62fac
Alwinriyas <alwinriyas16@gmail.com>
2026-08-23 11:29:58 +0530

    feat(realtime): pass Socket.IO to like events

--- server/src/modules/likes/likes.routes.js
+++ server/src/modules/likes/likes.routes.js
@@ -3,8 +3,7 @@ import { authenticate } from '../../middleware/authenticate.js'
 import { toggleLike } from './likes.service.js'
 
 export const likesRouter = Router()
-
 likesRouter.post('/:postId', authenticate, async (req, res, next) => {
-  try { res.status(200).json({ data: await toggleLike(req.user.id, req.params.postId) }) }
+  try { res.status(200).json({ data: await toggleLike(req.user.id, req.params.postId, req.app.get('io')) }) }
   catch (error) { next(error) }
 })
